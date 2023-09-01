69b35b0d18fc40e91b12b1c4d34d236dc12a32c2
amisalaam <amisalaam9@gmail.com>
2023-09-01 17:33:40 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {useState} from 'react'
+import Todo from './Components/Todo';
+
+function App() {
+  
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App;
